9ed7dfa5349ca83aae87a2c82043abbcfcd6f0b2
castlemin999 <castlemin999@gmail.com>
2023-04-17 23:51:52 +0900

    feat: docker-compose 기능 추가

---
+++ scripts/backend-deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# AWS EC2 docker 설치
+if ! type docker > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install -y docker-ce
+fi
+
+# AWS EC2 docker-compose 설치
+if ! type docker-compose > /dev/null
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+PROJECT_ROOT="/home/ubuntu/app/backend"
+JAR_FILE="$PROJECT_ROOT/backend-app.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# docker-compose spring 컨테이너 재실행
+echo "$TIME_NOW > docker-compose spring 컨테이너 재실행" >> $DEPLOY_LOG
+
+cd $PROJECT_ROOT
+sudo docker-compose up -d --build backend
